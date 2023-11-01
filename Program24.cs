f6094a71f85586e5d30d0bece69447037e098f60
Mughees-Yasir <146824921+Mughees-Yasir@users.noreply.github.com>
2023-11-01 09:57:29 +0500

    Add files via upload

---
+++ Program24.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+delegate void MDelegate();
+
+class DM
+{
+    public static void Display()
+    {
+        Console.WriteLine("New Delhi");
+    }
+
+    public static void Print()
+    {
+        Console.WriteLine("New York");
+    }
+}
+
+class MTest
+{
+    public static void Main()
+    {
+        MDelegate m1 = new MDelegate(DM.Display);
+        MDelegate m2 = new MDelegate(DM.Print);
+
+        MDelegate m3 = m1 + m2;
+        MDelegate m4 = m2 + m1;
+        MDelegate m5 = m3 - m2;
+
+        m3();
+        m4();
+        m5();
+    }
+}
