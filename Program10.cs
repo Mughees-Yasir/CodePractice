7912cea7552412b3ac7f0fa1fedfaa24035ea972
Mughees-Yasir <146824921+Mughees-Yasir@users.noreply.github.com>
2023-11-01 09:48:26 +0500

    Add files via upload

---
+++ Program10.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApplication3;
+class Sample
+{
+    public string param1, param2;
+    public Sample(string x, string y)
+    {
+        param1 = x;
+        param2 = y;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sample obj = new Sample("Welcome", "Asp.Net");
+        Sample obj1 = new Sample("Welcome1", "Asp.Net");
+        Console.WriteLine(obj.param1 + " to " + obj.param2);
+        Console.WriteLine(obj1.param1 + " to " + obj1.param2);
+        Console.ReadLine();
+    }
+}
