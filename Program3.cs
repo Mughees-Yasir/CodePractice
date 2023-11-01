51bd7da0dbdb50d6c6cabc5f698073120138a243
Mughees-Yasir <146824921+Mughees-Yasir@users.noreply.github.com>
2023-11-01 09:43:54 +0500

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace c
+{
+    class pro
+    {
+        public static void Main(String[] args)
+        {
+
+            int n, i = 0;
+            Console.WriteLine("enter n value");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                i++;
+                n = n / 10;
+            }
+            Console.WriteLine("Magnitude of given num is:" + i);
+        }
+    }
+}
