124fb31267e38d3b45249fa23ad4f40180e3bc9e
Mughees-Yasir <146824921+Mughees-Yasir@users.noreply.github.com>
2023-11-01 09:52:48 +0500

    Add files via upload

---
+++ Program16.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication8
+{ 
+class A
+{
+    public void display()
+    {
+        Console.WriteLine("hahahaha");
+    }
+}
+class B : A
+{
+    public void display1()
+    {
+        Console.WriteLine("hihihih");
+    }
+}
+class D : B
+{
+    public void display2()
+    {
+
+        Console.WriteLine("hohohohoh");
+    }
+}
+class C
+{
+    public static void Main()
+    {
+        D x = new D();
+        x.display();
+        x.display1();
+        x.display2();
+    }
+}
+}
