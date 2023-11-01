625714019073d93752021d836aeade84efcfee0e
Mughees-Yasir <146824921+Mughees-Yasir@users.noreply.github.com>
2023-11-01 09:50:40 +0500

    Add files via upload

---
+++ Program13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleApplication3;
+public class Sample
+{
+    public string param1, param2;
+    public Sample(string a, string b)
+    {
+        param1 = a;
+        param2 = b;
+    }
+    private Sample() // Private Constructor Declaration
+    {
+        Console.WriteLine("Private Constructor with no pramete");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sample obj = new Sample("Welcome", "to ASP.NET");
+        Console.WriteLine(obj.param1 + "" + obj.param2);
+        Console.ReadLine();
+    }
+}
