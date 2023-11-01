48b455a8dc9e76d1fe0f76fe02c5621793eed92b
Mughees-Yasir <146824921+Mughees-Yasir@users.noreply.github.com>
2023-11-01 09:42:32 +0500

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace prime
+{
+    class Pro
+    {
+        public static void Main()
+        {
+            int i, c, m = 1;
+            Console.WriteLine("Enter a value");
+            int n = int.Parse(Console.ReadLine());
+            c = 0; i = 1;
+            while (i <= n)
+            {
+                if (n % i == 0)
+                {
+                    if (i != n)
+                    {
+                        m = 1;
+                        c++;
+                    }
+                    i++;
+                }
+                if (c == 2)
+                    Console.WriteLine("num is prime");
+                else
+                    Console.WriteLine(m + "is largest factor for " + n);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
